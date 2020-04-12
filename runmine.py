4c57b1dbe2cc903277caeb409ecb2853f99e64ee
Nithin Mahesh <nmahesh@umass.edu>
2020-04-12 14:19:48 -0400

    Figured out that there was a timeout error with the page load and when the button was loaded. Issue is fixed now

--- runmine.py
+++ runmine.py
@@ -4,16 +4,23 @@ from selenium import webdriver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 # TODO figure out how to read webpage periodically from website
 
-# path to the chromedriver program
+# Path to the chromedriver program
 service = Service('C:\Program Files (x86)\Google\chromedriver.exe')
 service.start()
 
-# driver opens the remote with robinhood website
+# Driver opens the remote with robinhood website
 driver = webdriver.Remote(service.service_url)
-driver.get('https://robinhood.com/crypto/BTC');
-time.sleep(200)
+driver.get('https://robinhood.com/crypto/BTC')
 
-# closes the driver after timeout
-driver.quit()
+# We will grab the element id's to log on to Robinhood
+# driver.find_element_by_id(“ID”).send_keys(“username”)
+# driver.find_element_by_id (“ID”).send_keys(“password”)
+# driver.find_element_by_id(“submit”).click()
+WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='_3kh8OsNx6QdAbMaoKTi2Yq _1uaripz9PIQ8yApSTs6BKk']"))).click()
+
