6e1f709fb551b1566facbaf1c62cdda0b9770665
Nithin Mahesh <nmahesh@umass.edu>
2020-04-21 17:37:44 -0400

    Optimized clicks and added text search

--- sup.py
+++ sup.py
@@ -14,16 +14,19 @@ from selenium.webdriver.common.keys import Keys
 service = Service('C:\Program Files (x86)\Google\chromedriver.exe')
 service.start()
 
-# Driver opens the remote with robinhood website
 driver = webdriver.Remote(service.service_url)
 driver.get('https://www.supremenewyork.com/')
 
-# We will grab the element id's to log on to Robinhood
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+# driver.find_elements_by_xpath('/html/body/div[2]/nav/ul/li[4]/a').click()
+
+WebDriverWait(driver, 1).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/div[2]/nav/ul/li[4]/a'))).click()
 
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/footer/nav/ul[2]/li[1]/a'))).click()
 
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+WebDriverWait(driver, 1).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/div[2]/nav/ul/li[9]/a'))).click()    
+
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+    (By.XPATH, "//a[contains(text(),'Acid Yellow')]"))).click()   
