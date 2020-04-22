3002fd82d826a11ce8df6d9bf9b8885fa5fc4e4b
Nithin Mahesh <nmahesh@umass.edu>
2020-04-21 22:18:28 -0400

    Script works pretty well, gets me to checkout cart with information filled out in 11.52 seconds

--- sup.py
+++ sup.py
@@ -10,6 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+# Will eventually build this out to be more user friendly 
+# Enter Keywords:
+# TshirtName = input("Enter name of T-shirt: ")
+# color = input("Enter color: ")
+
 # Path to the chromedriver program
 service = Service('C:\Program Files (x86)\Google\chromedriver.exe')
 service.start()
@@ -22,11 +27,51 @@ driver.get('https://www.supremenewyork.com/')
 WebDriverWait(driver, 1).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/div[2]/nav/ul/li[4]/a'))).click()
 
-WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/footer/nav/ul[2]/li[1]/a'))).click()
 
 WebDriverWait(driver, 1).until(EC.element_to_be_clickable(
     (By.XPATH, '/html/body/div[2]/nav/ul/li[9]/a'))).click()    
 
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
-    (By.XPATH, "//a[contains(text(),'Acid Yellow')]"))).click()   
+# Need to figure out how to search by any keyword
+WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
+    (By.XPATH, " //*[contains(text(),'Morph')] /parent::div/following-sibling::*/*[contains(text(),'Black')]"))).click()
+
+WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
+    (By.XPATH, "//*[@id='add-remove-buttons']/input"))).click()
+
+WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
+    (By.XPATH, "/html/body/div[2]/div/div[1]/div/a[2]"))).click()
+
+# Enter name
+driver.find_element_by_css_selector('#order_billing_name').send_keys("Test Testing")
+
+# Enter Email
+driver.find_element_by_css_selector('#order_email').send_keys("NithisaSav@gmail.com")
+
+# Enter Phone
+driver.find_element_by_css_selector('#order_tel').send_keys("617-111-1330")
+
+# Enter Address
+driver.find_element_by_css_selector('#bo').send_keys("25 Testing Road")
+
+# Enter Zip
+driver.find_element_by_css_selector('#order_billing_zip').send_keys("05124")
+
+# Enter City
+driver.find_element_by_css_selector('#order_billing_city').send_keys("New York")
+
+# Enter State
+driver.find_element_by_css_selector('#order_billing_state').send_keys("MA")
+
+# Enter Credit Card Number:
+driver.find_element_by_css_selector('#rnsnckrn').send_keys("41000000000000000")
+
+# Enter Expiration Month
+driver.find_element_by_css_selector('#credit_card_month').send_keys("09")
+
+# Enter Expiration Year
+driver.find_element_by_css_selector('#credit_card_year').send_keys("2019")
+
+# Enter CVV
+driver.find_element_by_css_selector('#orcer').send_keys("696")
